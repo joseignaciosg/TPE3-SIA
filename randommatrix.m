32983c7866c5f837e6d3f56d2aaca61aaf419cd3
nloreti <nloreti@gmail.com>
2013-05-29 18:36:51 -0300

    subo random matirx

---
+++ randommatrix.m
@@ -0,0 +1,25 @@
+%creates a weigth matrix of the correct dimensions
+
+%P is the vector with the layers of the net
+function [W] = randommatrix(P,divisor,delta)
+
+    max_neurons = max(P);
+    m = length(P);
+    W = zeros(max_neurons,max_neurons+1,m-1);
+    
+    i=1;j=1;k=1;
+    while(k<m)
+        i=1;
+        while(i<=P(k+1))
+           j=1;
+           while(j<=P(k)+1)
+               W(i,j,k) = rand()/divisor - delta;
+               j=j+1;
+           end
+           i=i+1;
+        end
+        k=k+1;
+    end
+
+
+end
