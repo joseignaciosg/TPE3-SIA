4a2b404fd06d542034536bdf31552edde7603dcc
Chino <joseignaciosg9@gmail.com>
2013-06-05 22:54:54 -0300

    pot

--- operators/backpropagation.m
+++ operators/backpropagation.m
@@ -2,91 +2,95 @@
 %function [V,D,A,s,o,count,dif] = backpropagation( a )
 function [a] = backpropagation( a )
 
-%convierto el vector a matriz
-A = vec2mat(a);
+    %convierto el vector a matriz
+    A = vec2mat(a);
 
-global series;
-global P;
-global beta;
-eta = 0.045;
-epochs = 100;
-lrn_type = 3; %dinamico
- 
-%maximo valor de P para formar la matriz
-m = max(P);
+    global series;
+    global P;
+    global beta;
+    eta = 0.045;
+    epochs = 100;
+    lrn_type = 3; %dinamico
 
-
-windowsize = P(1);
-
-index = P(1) -1; %resto -1 para que de bien el index en el vector testing
-
-%Series a tomar en cuenta para entrenamiento
-max_serie = max(abs(series));
-series = series(1:750)./max_serie;
+    %maximo valor de P para formar la matriz
+    m = max(P);
 
 
-dif = 10;
-old = 11;
-errors = [];
-x = [];
-etas = [];
-os=[];
-ss=[];
-os_x=[0];
-count = 0;
+    windowsize = P(1);
 
-%Almacenamiento de Errores cuadraticos para realizar el dinamic learning rate.
-cuadratic_errors = 0;
-cuadratic_error = 0;
-contar = 0;
-jump = 0;
+    index = P(1) -1; %resto -1 para que de bien el index en el vector testing
+
+    %Series a tomar en cuenta para entrenamiento
+    max_serie = max(abs(series));
+    series = series(1:750)./max_serie;
 
 
+    dif = 10;
+    old = 11;
+    errors = [];
+    x = [1 ];
+    etas = [];
+    os=[];
+    ss=[];
+    os_x=[0];
+    count = 1;
 
-while(count < epochs && abs(dif-old) > 1e-10)
-	i=1;
-    %count
-	old = dif;
-	cuadratic_error = 0;
-	dif = 0;
-    os = [];
-    ss = [];
-    os_x = [];
-    [patterns]  = shufflePatterns(series,windowsize);
-    while(i<=size(patterns,1))
-        pattern = patterns(i,1:windowsize);
-        s = patterns(i,windowsize+1); 
-		[A,s,o] = variable3(pattern,A,s,eta);			
-		final_s = s * max_serie;
-		final_o = o * max_serie;
-		os = [final_o os];
-        ss = [final_s ss];
-        os_x = [i os_x];
-        i=i+1;
-        cuadratic_error = cuadratic_error + (s-o).^2;
-		dif = dif + (s-o).^2;
-    end
-    dif = dif / (i-1); % #patterns;
-    cuadratic_error = cuadratic_error/(i-1); % #patterns
-    errors = [dif errors];
-    x = [count x];
-    etas = [etas eta];
-   
-    cuadratic_errors = [cuadratic_errors cuadratic_error];
-
-    %Modifico el eta en caso de ser necesario
-    [eta, contar] = update_lrn_rate(lrn_type, eta, cuadratic_error, cuadratic_errors(length(cuadratic_errors)-1), contar, jump);
+    %Almacenamiento de Errores cuadraticos para realizar el dinamic learning rate.
+    cuadratic_errors = 0;
+    cuadratic_error = 0;
+    contar = 0;
+    jump = 0;
 
 
-    if(count > 50)
-        jump = ifErrorsAreSimilar(errors(1,1:50));
-    end
 
-    count = count+1;
+    while(count <= epochs && abs(dif-old) > 1e-10)
+        i=1;
+        %count
+        old = dif;
+        cuadratic_error = 0;
+        dif = 0;
+        os = [];
+        ss = [];
+        os_x = [];
+        [patterns]  = shufflePatterns(series,windowsize);
+        while(i<=size(patterns,1))
+            pattern = patterns(i,1:windowsize);
+            s = patterns(i,windowsize+1); 
+            [A,s,o] = variable3(pattern,A,s,eta);			
+            final_s = s * max_serie;
+            final_o = o * max_serie;
+            os = [final_o os];
+            ss = [final_s ss];
+            os_x = [i os_x];
+            i=i+1;
+            cuadratic_error = cuadratic_error + (s-o).^2;
+            dif = dif + (s-o).^2;
+        end
+        dif = dif / (i-1); % #patterns;
+        cuadratic_error = cuadratic_error/(i-1); % #patterns
+        errors = [dif errors];
+        x = [x count+1];
+        etas = [etas eta];
+        cuadratic_error
+        cuadratic_errors = [cuadratic_errors cuadratic_error];
+        %cuadratic_errors
+        %x
+        figure(3);
+        plot(x,cuadratic_errors);
+        
+        %Modifico el eta en caso de ser necesario
+        [eta, contar] = update_lrn_rate(lrn_type, eta, cuadratic_error, cuadratic_errors(length(cuadratic_errors)-1), contar, jump);
+
+
+        if(count > 50)
+            jump = ifErrorsAreSimilar(errors(1,1:50));
+        end
+
+        count = count+1;
     end
 
     a = mat2vec(A);
-    
+
     
 end
 
--- operators/mutar.m
+++ operators/mutar.m
@@ -9,7 +9,7 @@ while(i < l)
 	if(rand < pm)
 		%a(i) = rand * ( abs(max(a)) + abs(min(a)) ) - abs(min(a));
         a(i) = rand() - 0.5;
-	        mutation_counter = mutation_counter + 1;
+	     mutation_counter = mutation_counter + 1;
 	end
 
 	i = i + 1;
